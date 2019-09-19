1d814c2c91f9b7d48fa24dff32a2c72b1f94172e
Prajwal Prakash <prajwalpy6@gmail.com>
2019-09-19 14:15:32 -0400

    Start the node genservers first, use set_neighbours, add_neighbour(s) to setup the network topology and the initial working version of the Gossip protocol

--- lib/gossip_simulator/supervisor.ex
+++ lib/gossip_simulator/supervisor.ex
@@ -1,19 +1,17 @@
 defmodule GossipSimulator.Supervisor do
   use Supervisor
 
+  @default_node_state %{counter: 0, neighbours: []}
+
   def start_link(opts) do
     Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   @impl true
-  def init({num_nodes, topology}) do
+  def init(num_nodes) do
 
-    node_ids = 1..num_nodes    
-    topology = GossipSimulator.TopologyBuilder.build(node_ids, topology)
-
-    nodes = Enum.map(topology, fn i ->
-      {id, _} = i
-      Supervisor.child_spec({GossipSimulator.Node, i}, id: id)
+    nodes = Enum.map(1..num_nodes, fn i ->
+      Supervisor.child_spec({GossipSimulator.Node, @default_node_state}, id: i)
     end)
 
     Supervisor.init(nodes, strategy: :one_for_one)
