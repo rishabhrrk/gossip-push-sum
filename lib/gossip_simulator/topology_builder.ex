f0d19963937283cc22f3cbb9993b66feb54b9ae2
Prajwal Prakash <prajwalpy6@gmail.com>
2019-09-30 03:35:53 -0400

    Clean topology_builder.ex

--- lib/gossip_simulator/topology_builder.ex
+++ lib/gossip_simulator/topology_builder.ex
@@ -35,77 +35,114 @@ defmodule GossipSimulator.TopologyBuilder do
     end)
   end
 
-  defp calc_distance(a, b) do
-    c = a - b
-     
-    if c < 0 do
-      c * (-1)
-    else
-      c
-    end
+  defp calc_distance(x1, y1, x2, y2) do
+    x = :math.pow x1 - x2, 2
+    y = :math.pow y1 - y2, 2
+    :math.sqrt(x + y)
   end
 
   defp build_random2d(node_ids) do
-    cordinates = node_ids
-      |> Enum.map(fn {node_id} ->
-        {node_id, :rand.uniform(), :rand.uniform()}
-      end)
+    cordinates = Enum.map(node_ids, fn node_id ->
+      {node_id, :rand.uniform(), :rand.uniform()}
+    end)
 
-    for {nodei,xi, yi} <- cordinates do
-      neighbours = for {nodej,xj, yj} <- cordinates, nodei != nodej,
-          calc_distance(xi, xj) < 0.1, calc_distance(yi,yj) < 0.1 do
-                          {nodej}
-                    end
-      {nodei, neighbours}
+    for {node1, x1, y1} <- cordinates do
+      neighbours = for {node2, x2, y2} <- cordinates, node1 != node2, calc_distance(x1, y1, x2, y2) < 0.1 do
+        node2
+      end
+
+      {node1, neighbours}
     end
   end
 
   defp build_honeycomb(node_ids) do
     node_count = Enum.count(node_ids)
     row_length = round(:math.sqrt(node_count))
+
     node_ids
     |> Enum.with_index()
     |> Enum.map(fn {node_id, index} ->
       neighbours = cond do
+
         # 1 odd row, odd index
-         rem(div(index, row_length) + 1,2) != 0
-         && rem(rem(index,row_length),2) != 0
-         -> [Enum.at(node_ids, index - 1),
-              Enum.at(node_ids, index + row_length),
-              Enum.at(node_ids, index - row_length)]
+        rem(div(index, row_length) + 1, 2) != 0
+        && rem(rem(index,row_length), 2) != 0
+        -> [
+          Enum.at(node_ids, index - 1),
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
         # 2 odd row, even index, not last index
-          rem(div(index, row_length) + 1,2) != 0
-          && rem(rem(index,row_length),2) == 0
-          && (row_length - rem(index,row_length)) != 1
-          -> [Enum.at(node_ids, index + 1),
-               Enum.at(node_ids, index + row_length),
-               Enum.at(node_ids, index - row_length)]
-        # 3 even row , odd index
-          rem(div(index, row_length) + 1,2) == 0
-          && rem(rem(index,row_length),2) != 0
-          -> [Enum.at(node_ids, index + 1),
-                Enum.at(node_ids, index + row_length),
-                Enum.at(node_ids, index - row_length)]
-        # 4 even row, even index, not  first index
-          rem(div(index, row_length) + 1,2) == 0
-          && rem(rem(index,row_length),2) == 0
-          && rem(index,row_length) != 0
-          -> [Enum.at(node_ids, index - 1),
-                Enum.at(node_ids, index + row_length),
-                Enum.at(node_ids, index - row_length)]
+        rem(div(index, row_length) + 1, 2) != 0
+        && rem(rem(index,row_length), 2) == 0
+        && (row_length - rem(index,row_length)) != 1
+        -> [
+          Enum.at(node_ids, index + 1),
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        # 3 even row , odd index, not first index
+        rem(div(index, row_length) + 1, 2) == 0
+        && rem(rem(index,row_length), 2) != 0
+        && rem(index,row_length) != 0
+        -> [
+          Enum.at(node_ids, index + 1),
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        # 4 even row, even index, not first index
+        rem(div(index, row_length) + 1, 2) == 0
+        && rem(rem(index,row_length), 2) == 0
+        && rem(index,row_length) != 0
+        -> [
+          Enum.at(node_ids, index - 1),
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
         # 5 even row, even index, first index
-        rem(div(index, row_length) + 1,2) == 0
-        && rem(rem(index,row_length),2) == 0
+        rem(div(index, row_length) + 1, 2) == 0
+        && rem(rem(index,row_length), 2) == 0
         && rem(index,row_length) == 0
-        -> [  Enum.at(node_ids, index + row_length),
-              Enum.at(node_ids, index - row_length)]
-        # 6 odd row, odd index, last member
-        rem(div(index, row_length) + 1,2) != 0
-        && rem(rem(index,row_length),2) != 0
+        -> [
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        # 6 even row, odd index, first index
+        rem(div(index, row_length) + 1, 2) == 0
+        && rem(rem(index,row_length), 2) != 0
+        && rem(index,row_length) == 0
+        -> [
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        # 7 odd row, odd index, last member
+        rem(div(index, row_length) + 1, 2) != 0
+        && rem(rem(index,row_length), 2) != 0
         && (row_length - rem(index,row_length)) == 1
-        -> [ Enum.at(node_ids, index + row_length),
-             Enum.at(node_ids, index - row_length)]
+        -> [
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        # 8 odd row, even index, last member
+        rem(div(index, row_length) + 1, 2) != 0
+        && rem(rem(index,row_length), 2) == 0
+        && (row_length - rem(index,row_length)) == 1
+        -> [
+          Enum.at(node_ids, index + row_length),
+          Enum.at(node_ids, index - row_length)
+        ]
+
+        true -> Logger.error "Generating Honeycomb Topology
+        No match for index: #{index} and row length: #{row_length}"
       end
+
       {node_id, Enum.filter(neighbours, &(&1))}
     end)
   end
